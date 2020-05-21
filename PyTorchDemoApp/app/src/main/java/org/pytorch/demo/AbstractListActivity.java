b074ac936c9c6ca216ae7950216b469d0cd45f9e
monologg <adieujw@gmail.com>
2020-05-22 06:26:10 +0900

    Erase go back icon (It's not iPhone)

--- PyTorchDemoApp/app/src/main/java/org/pytorch/demo/AbstractListActivity.java
+++ PyTorchDemoApp/app/src/main/java/org/pytorch/demo/AbstractListActivity.java
@@ -12,7 +12,6 @@ public abstract class AbstractListActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_list_stub);
-    findViewById(R.id.list_back).setOnClickListener(v -> finish());
     final ViewStub listContentStub = findViewById(R.id.list_content_stub);
     listContentStub.setLayoutResource(getListContentLayoutRes());
     listContentStub.inflate();
